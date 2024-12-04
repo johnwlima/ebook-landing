8e2f76f60ea5403960acf81f10e65b847249d474
John Lima <jwesley1210@gmail.com>
2024-12-04 16:52:30 -0300

    feat: add dinamic chapters

--- src/lib/components/ChapterPreview.svelte
+++ src/lib/components/ChapterPreview.svelte
@@ -1,21 +1,88 @@
+<script>
+  const chapters = [
+    {
+      number: 1,
+      title: "Preparing for the Move",
+      strapline: "Navigating Bureaucracy",
+      excerpt:
+        "Moving to Spain involves navigating a labyrinth of bureaucratic processes that can feel overwhelming at first. This chapter breaks down the essential steps required to secure your visa, establish residency, and handle other legal formalities.",
+    },
+    {
+      number: 2,
+      title: "Finding Your Dream Home",
+      strapline: "From Coastal Villas to Urban",
+      excerpt:
+        "Finding the right home in a new country can be a challenge. This chapter guides you through the Spanish real estate market, including renting and buying properties, tips on different neighborhoods, and dealing with contracts and real estate agents.",
+    },
+    {
+      number: 3,
+      title: "Work and Career in Spain",
+      strapline: "Unlocking Opportunities",
+      excerpt:
+        "Exploring the job market and career opportunities in Spain is crucial for your integration and success. This chapter covers how to find a job, prepare your resume, interviews, and how to adapt to the Spanish work environment.",
+    },
+    {
+      number: 4,
+      title: "Social and Cultural Integration",
+      strapline: "Building Connections",
+      excerpt:
+        "Integrating into a new culture can be exciting and challenging. This chapter provides tips on making friends, understanding and respecting social norms, and getting involved in community activities to enrich your life experience in Spain.",
+    },
+    {
+      number: 5,
+      title: "Exploring Education and Culture",
+      strapline: "Learning and Immersion",
+      excerpt:
+        "Education in Spain offers a wide range of opportunities, from public schools to renowned higher education institutions. This chapter covers the education system, options for learning Spanish, and cultural immersion through museums, festivals, and local events.",
+    },
+    {
+      number: 6,
+      title: "Health and Well-being in Spain",
+      strapline: "Ensuring a Healthy Life",
+      excerpt:
+        "Understanding the Spanish healthcare system is crucial for your stay. This chapter covers everything from obtaining health insurance to accessing medical services and maintaining a healthy and balanced life in the new country.",
+    },
+    {
+      number: 7,
+      title: "Exploring the Spanish Territory",
+      strapline: "Discovering Regions and Natural Wonders",
+      excerpt:
+        "From vibrant urban life to serene rural landscapes, exploring Spain is an ongoing adventure. This chapter guides you through the most iconic regions, highlighting must-see tourist spots, local cuisine, and travel tips within the country.",
+    },
+  ];
+</script>
+
 <section class="chapter-preview default-margin">
   <h2 class="mb-l">What you're getting</h2>
   <div class="chapter-container">
     <ul>
-      <li>
-        <button class="chapter-title"
-        aria-controls="chapter-info-1"
-        aria-expanded="true">
-          chapter 1: Whatever
-        </button>
-      </li>
+      {#each chapters as chapter}
+        <li>
+          <button
+            class="chapter-title"
+            aria-controls={`chapter-info-${chapter.number}`}
+            aria-expanded="true"
+          >
+            Chapter {chapter.number}: {chapter.title}
+          </button>
+        </li>
+      {/each}
     </ul>
     <div class="chapter-info">
       <h3 class="chapter-strapline italic mb-s">
         Understanding Visas, Residency, and legal Essentials
       </h3>
       <p>
-        Moving to Spain involves navigating a labyrinth of bureaucratic processes that can feel overwhelming at first. This chapter breaks down the essential steps required to secure your visa, establish residency, and handle other legal formalities. Whether you're planning a short-term stay or seeking permanent residency, understanding Spain's legal framework is crucial. We'll explore the different types of visas available, from student to work visas, and provide tips on how to streamline your application process. Additionally, this chapter covers essential documentation, such as obtaining a Número de Identidad de Extranjero (NIE), which is your key to unlocking many services in Spain.
+        Moving to Spain involves navigating a labyrinth of bureaucratic
+        processes that can feel overwhelming at first. This chapter breaks down
+        the essential steps required to secure your visa, establish residency,
+        and handle other legal formalities. Whether you're planning a short-term
+        stay or seeking permanent residency, understanding Spain's legal
+        framework is crucial. We'll explore the different types of visas
+        available, from student to work visas, and provide tips on how to
+        streamline your application process. Additionally, this chapter covers
+        essential documentation, such as obtaining a Número de Identidad de
+        Extranjero (NIE), which is your key to unlocking many services in Spain.
       </p>
     </div>
   </div>
