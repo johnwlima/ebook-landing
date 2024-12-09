1dd4f0c40c214f988b8203ccaa8d76b2a432bf06
John Lima <jwesley1210@gmail.com>
2024-12-08 21:21:58 -0300

    doc: rewrite failure page content

--- src/routes/checkout/failure/+page.svelte
+++ src/routes/checkout/failure/+page.svelte
@@ -1,28 +1,29 @@
 <script>
-    import { HeroSection, Button } from "$components";
-  </script>
-  
-  <HeroSection>
-    <h1>ERROR WHILE</h1>
-    <h1 class="mb-l">PURCHASING</h1>
-  
-    <p class="light-grey mb-s">
-      You've made a great decision by choosing this guide to help you on your
-      journey to Spain. We're confident that the insights and advice contained in
-      this ebook will save you time, money, and unnecessary stress as you plan
-      your move.
-    </p>
-    <p class="light-grey mb-s">
-      An email with your ebook is on its way to your inbox, along with a separate
-      purchase confirmation. Please keep an eye out for the email. If you don't
-      see them within the next few minutes, be sure to check your spam or
-      promotions folder, just in case.
-    </p>
-    <p class="light-grey mb-m">
-      We're here to support you every step of the way. If you have any questions
-      or need further assistance, don't hesitate to reach out. Enjoy your read and
-      best of luck with your move!
-    </p>
-    <Button>Try again</Button>
-    </HeroSection
-  >
+  import { HeroSection, Button } from "$components";
+</script>
+
+<HeroSection>
+  <h1>PURCHASE ERROR</h1>
+  <h1 class="mb-l">PLEASE TRY AGAIN</h1>
+  <p class="light-grey mb-s">
+    Oops! Something went wrong while processing your purchase. We apologize for
+    the inconvenience.
+  </p>
+  <p class="light-grey mb-s">
+    Don't worry, your interest in the Complete Spain Relocation Guide hasn't
+    gone unnoticed. This guide is packed with insights and advice that can help
+    you save time, money, and avoid unnecessary stress during your move to
+    Spain.
+  </p>
+  <p class="light-grey mb-s">
+    Please try completing your purchase again by clicking the button below. If
+    the issue persists, make sure to check your internet connection or try using
+    a different payment method.
+  </p>
+  <p class="light-grey mb-m">
+    We're here to support you. If you continue to experience issues or have any
+    questions, feel free to reach out to our support team at
+    <a href="mailto:mail@mail">mail@mail.</a> We're more than happy to assist you.
+  </p>
+  <Button>Try Again</Button>
+</HeroSection>
